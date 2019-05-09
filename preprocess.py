df3c2463a8edadea1c2948d611600a72db744db7
Nikku Chatha <nchatha@ckmadvisors.com>
2019-05-08 20:40:19 -0400

    helper functions for cleaning hpi and interest

--- preprocess.py
+++ preprocess.py
@@ -3,20 +3,24 @@ import pandas as pd
 import numpy as np
 
 ####filepaths
-interest_15 = "data/primary15yr.txt"
-interest_30 = "data/primary30yr.txt"
-state_hbi = "data/state_hbi.csv"
-state_uer = "data/state_uer.csv"
-uer = "UnemploymentHistory.txt"
-###delimiters differ by file :rage:
+#interest_15 = "data/primary15yr.txt"
+#interest_30 = "data/primary30yr.txt"
+#state_hbi = "data/state_hbi.csv"
+#state_uer = "data/state_uer.csv"
+#uer = "UnemploymentHistory.txt"
 
-class Preprocess():
 
-	def __init__(self):
-		pass
+def clean_interest(df):
+	df.index = map(lambda x: x[-4:] + x[:2], df["Date"])
+	df.index = df.index.astype(int)
+	df.drop('Date', axis=1, inplace=True)
+	return df
 
-	def clean_interest(df):
-		df.index = map(lambda x: x[-4:] + x[:2], df["Date"])
-		df.index = df.index.astype(int)
-		df.drop('Date', axis=1, inplace=True)
-		return df
+def clean_hpi(df):
+	df['Date'] = df['Date'].apply(lambda x: x[3:] + x[:2])
+	df['Date'] = df['Date'].astype(int, copy=False)
+	df.set_index('Date', inplace = True)
+	return df
+
+def calculate_hpa(hpi):
+
